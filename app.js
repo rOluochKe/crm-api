3db38605b2cf34c6d4ab60c41a2054fd1cbb5020
roluochKe <rasholuoch@gmail.com>
2021-11-25 20:13:21 +0300

    refreshjwt token

--- app.js
+++ app.js
@@ -46,10 +46,12 @@ app.use(bodyParser.json())
 // load routers
 const userRouter = require('./src/routers/user.router')
 const ticketRouter = require('./src/routers/ticket.router')
+const tokensRouter = require('./src/routers/tokens.router')
 
 // use routes
 app.use('/v1/user', userRouter)
 app.use('/v1/ticket', ticketRouter)
+app.use('/v1/tokens', tokensRouter)
 
 // error handling
 const handleError = require('./src/utils/errorHandler')
